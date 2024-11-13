ea21e5ba8efb1e36f95a3d236da3dd9c446dd38a
Abdullah Atif <abdullahatif721@outlook.com>
2024-11-13 22:02:33 +0600

    updateProblem2

--- Lab reports/Lab Report 08 07.11.2024/Problem2findMax.cpp
+++ Lab reports/Lab Report 08 07.11.2024/Problem2findMax.cpp
@@ -12,26 +12,23 @@ public:
   float findMax(float n1, float n2) {
     p = n1;
     q = n2;
-    float result = max(p, q);
 
-    return result;
+    return max(p, q);
   }
 
   int findMax(int n1, int n2) {
     a = n1;
     b = n2;
-    int result = max(a, b);
 
-    return result;
+    return max(a, b);
   }
 
   int findMax(int n1, int n2, int n3) {
     x = n1;
     y = n2;
     z = n2;
-    int result = max(x, max(y, z));
 
-    return result;
+    return max(x, max(y, z));
   }
 };
 
@@ -40,9 +37,9 @@ int main() {
 
   FindMaxNumber obj1, obj2, obj3;
 
-  cout << "Maximum of two Integers :" << obj1.findMax(1211.16f, 325.41f) << endl;
-  cout << "Maximum of two floating-point numbers :"<<obj2.findMax(11, 22) << endl;
-  cout << "Maximum of three Integers :"<<obj3.findMax(12, 25, 30) << endl;
-  
+  cout << "Maximum of two Integers :" << obj1.findMax(11.16f, 325.41) << endl;
+  cout << "Maximum of two floating-point numbers :" << obj2.findMax(11, 22) << endl;
+  cout << "Maximum of three Integers :" << obj3.findMax(12, 25, 30) << endl;
+
   return 0;
 }
